8952f4afebcd3406cd2f25bdb5cb3ec47e1de2b6
Miguel Martinez <miguelitalia@hotmail.com>
2018-06-13 16:04:32 +0000

    registration form amended to include two passwords and form file created to start form functions

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def register(request):
-    return render(request, "accounts/register.html")
+   return render(request, "accounts/register.html")
+    

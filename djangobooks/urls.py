0d923516e4bc3f21090c4af309473ec4ad910ebf
Miguel Martinez <miguelitalia@hotmail.com>
2018-06-13 14:45:38 +0000

    got landing page to render with navbar included

--- djangobooks/urls.py
+++ djangobooks/urls.py
@@ -1,4 +1,4 @@
-"""books URL Configuration
+"""djangobooks URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from books.views import get_index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r"^$", get_index)
 ]
